525f11c752f46f3689a3cf88841c60df8bca4b1f
Adnan <adnanali23189@gmail.com>
2024-08-25 22:57:49 +0530

    Light mode Service Section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Service from "@/components/Service";
 import Background from "@/components/ui/Background";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
     <h1>This is a Template branch and all the templates which are not used in development phase are here which can be used in future</h1>
     <Background/>
+    <Service/>
     </>
   );
 }
