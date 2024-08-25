3435922ee577e0fe6722698ac5820ad5268b1c46
Adnan <adnanali23189@gmail.com>
2024-08-25 17:59:36 +0530

    Basic Service Section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/Header";
+import Service from "@/components/Service";
 
 
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
       <>
       <Header/>
+      <Service/>
       </>
   );
 }
