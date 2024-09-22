8d2a729b82a78fd9d4fdd7c7ebb15f1d6c650917
Adnan <adnanali23189@gmail.com>
2024-09-22 10:23:16 +0530

    Home Page SEO Optimization

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,8 +2,27 @@ import type { ReactNode } from 'react';
 import './home.css';
 
 export const metadata = {
-  title: "Electroplix",
-  description: "Crafting Outstanding Digital Experiences",
+  title: "Electroplix - Scale Your Business with Innovative Web Solutions",
+  description: "Electroplix offers custom web development, SEO, digital marketing, and mobile app solutions. Scale your business with our web components, premium templates, and expert services.",
+  keywords: "web development, SEO, digital marketing, web components, mobile apps, social media marketing, Electroplix, custom templates, hosting, responsive design, scalable solutions",
+  author: {
+    name: "Syed Adnan Ali",
+    url: "https://www.linkedin.com/in/syedadnanali99"
+  },
+  og: {
+    title: "Electroplix - Scale Your Business",
+    description: "Join Electroplix to empower your business with cutting-edge web solutions and services tailored for growth.",
+    url: "https://www.electroplix.com",
+    type: "website",
+    image: "https://www.electroplix.com/assets/Electroplix_logo.jpeg", // Replace with your logo or relevant image URL
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@Electroplix_", // Replace with your Twitter handle
+    title: "Electroplix - Scale Your Business",
+    description: "Empower your business with Electroplix's innovative web solutions.",
+    image: "https://www.electroplix.com/assets/Electroplix Cover Photo.png", // Replace with your logo or relevant image URL
+  },
 };
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
