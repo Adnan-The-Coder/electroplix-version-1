1404f2fa028921d1e1a94538cec99673c0fef936
Adnan <adnanali23189@gmail.com>
2025-05-10 10:50:36 +0530

    test: test.electroplix.com production stage

--- components/Header.tsx
+++ components/Header.tsx
@@ -39,7 +39,7 @@ export function HeroSection() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: 0.5, ease: "easeOut" }}
           >
-            Crafting Outstanding Digital Experiences
+            Crafting Outstanding Digital Experiences.
           </motion.h2>
           {/* Short Description */}
           <motion.p
